2b185666ee65b7a907095d33f319147451b88931
Aleksey <andreev19848484@gmail.com>
2025-05-17 20:58:27 +0300

    ipr

--- script.js
+++ script.js
@@ -37,7 +37,29 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+const slides = document.querySelectorAll('.slide');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let index = 0;
 
+function showSlide(i) {
+  slides.forEach((slide, idx) => {
+    slide.classList.remove('active');
+    if (idx === i) slide.classList.add('active');
+  });
+}
+
+prev.addEventListener('click', () => {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+});
+
+next.addEventListener('click', () => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+});
+
+setInterval(() => next.click(), 5000);
 //.............................. модальное окно заказа звонка
 
 $(function(){
@@ -67,6 +89,7 @@ $(function(){
 	});
 	
 });
+
 
 
 // ...........................меню бургер..............
